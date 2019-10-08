71affa5f300ddf2321f2b06c41c587b2e1c46910
60142244_김명진 <kiakd2@gmail.com>
2019-10-08 19:02:13 +0900

    make Engineering element

---
+++ app/src/main/java/com/example/test5/ui/engineering/EngineeringFragment.java
@@ -0,0 +1,23 @@
+package com.example.test5.ui.engineering;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.test5.R;
+
+public class EngineeringFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return rootView;
+    }
+}
